08fbd517c013c8c45f80c7fdf843c89e464d9901
RauhoferE <104609@fhwn.ac.at>
2020-06-18 15:24:27 +0200

    Pushed Path

--- __init__.py
+++ __init__.py
@@ -224,11 +224,11 @@ class Helperbot(MycroftSkill):
         # 3 =  Enum for Picture Width
         cam.set(cv2.cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)  # I have found this to be about the highest-
         cam.set(cv2.cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
-
+        path = "/Temp/"
         if cam.isOpened():
             _, frame = cam.read()
             now = datetime.datetime.now()
-            file_name = now.strftime("%m_%d_%Y.%H_%M_%S") + ".jpg"
+            file_name = path + now.strftime("%m_%d_%Y.%H_%M_%S") + ".jpg"
             cam.release()
             cv2.cv2.imwrite(file_name, frame)
 
