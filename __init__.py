d35e2cc8d39fb0f0a222dac5bc94b4f622ca11b9
RauhoferE <104609@fhwn.ac.at>
2020-06-22 14:26:06 +0200

    Sending SMS

--- __init__.py
+++ __init__.py
@@ -241,9 +241,11 @@ class Helperbot(MycroftSkill):
         #     cv2.cv2.cv.imwrite(file_name, frame)
         pass
     
+    # Sends an SMS to the list of receivers.
     def sendSMS(self,message,numbers):
             # the following line needs your Twilio Account SID and Auth Token
-        client = Client("AC3aede748210f547a02e9d406efcadcbb", "31ae8c13f7a19e960e14369a8177417f")
+        client = Client("AC3aede748210f547a02e9d406efcadcbb", 
+        "31ae8c13f7a19e960e14369a8177417f")
 
     # change the "from_" number to your Twilio number and the "to" number
     # to the phone number you signed up for Twilio with, or upgrade your
